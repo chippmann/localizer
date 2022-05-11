90bb6cbab698566b4ba962b8e9eb60d5252c6b62
Cedric Hippmann <cedric.hippmann@hotmail.com>
2022-05-11 12:15:02 +0200

    Implement automatic changelog creation and draft release creation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.ajoberstar.grgit.Grgit
 
 plugins {
     kotlin("jvm") version "1.6.20"
@@ -5,6 +6,8 @@ plugins {
     `maven-publish`
     // https://plugins.gradle.org/plugin/com.gradle.plugin-publish
     id("com.gradle.plugin-publish") version "0.18.0"
+    // https://github.com/ajoberstar/grgit/releases
+    id("org.ajoberstar.grgit") version "4.1.1" //TODO: update to 5.0.0
 }
 
 group = "ch.hippmann"
@@ -54,9 +57,65 @@ java {
     withSourcesJar()
 }
 
+val grGit: Grgit = Grgit.open(mapOf("currentDir" to project.rootDir))
+
 tasks {
     build {
         finalizedBy(publishToMavenLocal)
+    }
+
+    @Suppress("UNUSED_VARIABLE") // used by github actions
+    val generateChangelog by creating {
+        group = "localizer"
+
+        doLast {
+            val tags = grGit.tag.list().reversed()
+            println("INFO: Got ${tags.size} tags")
+
+            val fromCommit = tags
+                .getOrNull(1)
+                ?.commit
+                ?.id
+                ?: grGit.log().last().id
+            val toCommit = tags
+                .getOrNull(0)
+                ?.commit
+                ?.id
+                ?.let { commitId ->
+                    if (commitId != fromCommit) {
+                        commitId
+                    } else {
+                        grGit.log().first().id
+                    }
+                } ?: grGit.log().first().id
+
+            println("INFO: Generate changelog; fromCommit: $fromCommit, toCommit: $toCommit")
+
+            val changelogString = grGit
+                .log {
+                    range(fromCommit, toCommit)
+                }
+                .also {
+                    println("INFO: Generate changelog; found commits in range: ${it.size}")
+                }
+                .joinToString(
+                    separator = "\n", prefix = """
+                        You can find the artifact on the [Gradle Plugin Portal](https://plugins.gradle.org/plugin/ch.hippmann.localizer)
+                        
+                        **Changelog:**
+                        
+                    """.trimIndent()
+                ) { commit ->
+                    val link = "https://github.com/chippmann/localizer/commit/${commit.id}"
+                    "- [${commit.abbreviatedId}]($link) ${commit.shortMessage}"
+                }
+
+            project.buildDir.resolve("changelog.md").also {
+                if (!it.parentFile.exists()) {
+                    it.parentFile.mkdirs()
+                }
+            }.writeText(changelogString)
+        }
     }
 }
 
